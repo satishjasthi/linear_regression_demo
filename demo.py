17204f3818341c18389dbd28212059d20cf2f363
satishjasthi <satish27may@gmail.com>
2017-01-17 14:34:52 +0530

    remove code related to training LR using single feature

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import cross_val_predict
 from sklearn.cross_validation import cross_val_score
-from IPython.display import display
+
 #read data
 dataframe = pd.read_csv('challenge_dataset.txt',names = ['Brain','Body'])
 features = dataframe
@@ -20,36 +20,11 @@ lr = LinearRegression()
 predicted = cross_val_predict(lr,features,target,cv = 5)
 scores = cross_val_score(lr,features,target,cv=5,scoring = 'r2')
 scores
+
+
 fig,ax = plt.subplots()
 ax.scatter(target,predicted)
 ax.plot([target.min(),target.max()],[target.min(),target.max()],'k--',lw=4)
 ax.set_xlabel("Features")
 ax.set_ylabel("Body weight")
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-print np.size(dataframe,axis=0)
-x_values = dataframe[['Brain']]
-y_values = dataframe[['Body']]
-
-#train model on data
-body_reg = linear_model.LinearRegression()
-body_reg.fit(x_values, y_values)
-
-#visualize results
-plt.scatter(x_values, y_values)
-plt.plot(x_values, body_reg.predict(x_values))
-plt.show() '''
